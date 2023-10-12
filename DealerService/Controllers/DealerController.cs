1fa51b62b3137cf38d693ad6534f291f717b4a38
David Lambert <dlambert@appdev.info>
2023-10-12 18:18:17 -0400

    Add project files.

---
+++ DealerService/Controllers/DealerController.cs
@@ -0,0 +1,120 @@
+﻿using DealerService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using System.Net;
+using System.Text.Json;
+
+namespace DealerService.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    //[Route("[controller]/[action]")]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class DealerController : ControllerBase
+    {
+        private List<Dealer> _sampleData = new List<Dealer>();
+
+        public DealerController() 
+        {
+            string json = System.IO.File.ReadAllText("./Data/sampleDealers.json");
+            _sampleData = JsonSerializer.Deserialize<List<Dealer>>(json);
+        }
+
+        // GET: DealerController
+        [HttpGet(Name = "GeDealer")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Dealer>))]
+        public IActionResult Get()
+        {
+            return Ok(_sampleData.ToArray());
+        }
+
+        // GET: DealerController/Details/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Dealer))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Details(int id)
+        {
+            Dealer? dealer = _sampleData.SingleOrDefault(d => d.Id == id);
+            return dealer == null ? NotFound() : Ok(dealer);
+        }
+
+        ////GET: DealerController/Create
+        //public Dealer Create()
+        //{
+        //    return new Dealer() { Description = "new dealer" };
+        //}
+
+        //POST: new dealer
+        [HttpPost]
+        public ActionResult CreateDealer(Dealer dealer)
+        {
+            if (dealer.Id > 0) // validation failed
+            {
+                return BadRequest("Do not supply ID for new entity.");
+            }
+
+            _sampleData.Add(dealer);  // doesn't really pay until we pull the data structure out of here
+            //SaveChangesAsync();
+
+            return CreatedAtAction(nameof(CreateDealer), new { id = dealer.Id }, dealer);
+        }
+
+        //PUT: edit existing dealer
+        [HttpPut("{id}")]
+        public ActionResult<Dealer> EditDealer(int id, Dealer dealer)
+        {
+            if (dealer.Id == 0 || id != dealer.Id) // validation failed
+            {
+                return BadRequest("Please supply ID for edit.");
+            }
+            Dealer? founddealer = _sampleData.SingleOrDefault(d => d.Id == dealer.Id);
+            if (founddealer == null)
+                return NotFound();
+
+            founddealer = dealer;
+            //SaveChangesAsync();
+
+            return founddealer;
+        }
+
+
+        // DELETE: Delete an existing Dealer
+        [HttpDelete("{id}")]
+        public ActionResult DeleteDealer(int id)
+        {
+            if (id == 0) // validation failed
+            {
+                return BadRequest("Please supply ID for delete.");
+            }
+            Dealer? founddealer = _sampleData.SingleOrDefault(d => d.Id == id);
+            if (founddealer == null)
+                return NotFound();
+
+            _sampleData.Remove(founddealer);
+            return Ok();
+        }
+
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        // POST: DealerController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ DealerService/Models/Dealer.cs
@@ -0,0 +1,10 @@
+﻿namespace DealerService.Models
+{
+    public class Dealer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Website {  get; set; }
+    }
+}
